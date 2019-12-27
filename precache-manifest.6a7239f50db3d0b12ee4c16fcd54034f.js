a8e93d9c0f63c5edc8eaf2dc08b50d003a5111ae
thegirlnotknownn <jainamisha.j@gmail.com>
2019-12-28 04:26:50 +0530

    Updates

---
+++ precache-manifest.6a7239f50db3d0b12ee4c16fcd54034f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2bcd4a8c3a1376ae8b02bdc3dc61d4",
+    "url": "/React-/index.html"
+  },
+  {
+    "revision": "262961bdfefc399cf941",
+    "url": "/React-/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e41e528458dd5fe03545",
+    "url": "/React-/static/js/2.cdcc2264.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-/static/js/2.cdcc2264.chunk.js.LICENSE"
+  },
+  {
+    "revision": "262961bdfefc399cf941",
+    "url": "/React-/static/js/main.0e9dd186.chunk.js"
+  },
+  {
+    "revision": "2319502aa2c112cd2169",
+    "url": "/React-/static/js/runtime-main.9a0d9bea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-/static/media/logo.5d5d9eef.svg"
+  }
+]);
